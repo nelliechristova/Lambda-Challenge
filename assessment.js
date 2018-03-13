9031415252bc6cdc423f1bd684820cb0894ed8e1
Nellie Christova <Nellie@Nelis-MacBook-Air.local>
2018-03-13 18:36:05 -0500

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,9 +7,9 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+  var message = "Hello World!";
+  return message;
 }
-
 /*
 2. Write a function called lambdaSchool that has a single parameter called num.
      num will be a positive integer.
@@ -23,10 +23,17 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
-}
-
+function lambdaSchool(num) {
+  if (num%3===0 && num%5===0){
+    return("Lambda School");
+  }else if (num%3===0 && num%5!==0){
+    return("Lambda");
+  }else if (num%3!==0 && num%5===0){
+    return("School");
+  }else{
+    return num;
+  }
+};
 /*
 3. Write a function called longestString that has a single parameter called strs.
      strs will be an array of strings.
@@ -38,10 +45,15 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
-
-}
-
+function longestString(strs) {
+  var max=strs[0];
+  for (var i = 0; i<strs.length; i++){
+    if(max.length<strs[i].length){
+      max = strs[i];
+    }
+  }
+    return max;
+  };
 /*
 4. Write a function called computeUserAverageAge that has a single parameter called users
      users is an array of user objects.
@@ -63,9 +75,14 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
-
+function computeUserAverageAge(users) {
+var sum = 0;
+for (var i = 0; i<users.length; i++){
+  sum = sum + users[i].age;
 }
+var avg = sum/users.length;
+return Math.round(avg);
+};
 
 module.exports = {
  helloWorld,
